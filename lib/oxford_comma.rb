985866a219cac1552da5121db361169129adfc83
Ay Has <ahashim983@yahoo.com>
2019-03-28 23:52:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join("")
+  end
+  array.push("and")
+  array[array.length-1],array[array.length-2] = array[array.length-2],array[array.length-1]
+  a = array.join(", ").sub!(', and',' and')
+  return a
 end
+
+#oxford_comma(["a","b"])
