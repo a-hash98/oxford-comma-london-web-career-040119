4f78eb976ca269b674650f24d26de59f91a1c6c1
Ay Has <ahashim983@yahoo.com>
2019-03-28 23:57:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,15 @@
 def oxford_comma(array)
+  len = array.length
   if array.length == 1
     return array.join(" ")
   end
   array.push("and")
   array[array.length-1],array[array.length-2] = array[array.length-2],array[array.length-1]
   a = array.join(", ")
-  return a.reverse.sub(',','').reverse
+  if len >= 3
+    return a.reverse.sub(',','').reverse
+  else
+    return a.sub(',','')
 end
 
 #oxford_comma(["a","b"])
